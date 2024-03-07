20ae71c32f9d0f7acce252bb2a2ade1e19f73af1
CamiloGz001 <camilogomez1014@hotmail.com>
2024-03-07 14:18:50 -0500

    Energy dep,resto
    uso y recuperacion de neergya alpha

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,11 +9,16 @@ public class PlayerController : MonoBehaviour
     private Rigidbody playerRb;
     public float speed = 500;
     private GameObject focalPoint;
+    public int maxEnergy = 100;
+    public int currentEnergy;
+    public Energy energyBar;
     // Start is called before the first frame update
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
         focalPoint = GameObject.Find("Focal Point");
+        currentEnergy = maxEnergy;
+        energyBar.SetMaxEnergy(maxEnergy);
     }
 
     // Update is called once per frame
@@ -22,7 +27,37 @@ public class PlayerController : MonoBehaviour
         float forwardInput = Input.GetAxis("Vertical");
         float horizontalInput = Input.GetAxis("Horizontal");
         playerRb.AddForce(focalPoint.transform.forward * forwardInput * speed);
-        playerRb.AddForce(focalPoint.transform.right * speed * horizontalInput);   
+        playerRb.AddForce(focalPoint.transform.right * speed * horizontalInput); 
+
+        
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            speed=1000;
+            playerRb.AddForce(focalPoint.transform.forward * speed * Time.deltaTime);
+            GetTired();
+        }
+        Recover();
+        speed=500;
+        
+        
          
+    }
+
+    void GetTired(){
+        for(int i=0;i<maxEnergy;i++)
+        {
+            currentEnergy -=i;
+            energyBar.SetEnergy(currentEnergy);
+            if(currentEnergy== 0){
+                speed = 250;
+            }
+        }
+    }
+    void Recover(){
+        for(int i=0;i<maxEnergy;i++)
+        {
+            currentEnergy +=i;
+            energyBar.SetEnergy(currentEnergy);
+        }
     }
 }
