7dab97842f02fb3be3845cb27449107d53e6fd97
Nicolas Parroquiano <nicolasparroquiano@gmail.com>
2024-03-07 15:54:03 -0500

    Animaciones del player dentro de player controller

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,15 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using System;
+using UnityEditor.Experimental.GraphView;
 
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody playerRb;
-    public float speed = 500;
+    public float speed;
+    public float initialSpeed =500;
     private GameObject focalPoint;
     public int maxEnergy = 100;
     public int currentEnergy;
     public Energy energyBar;
+    [SerializeField] private Animator playerAnim;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,17 +22,15 @@ public class PlayerController : MonoBehaviour
         focalPoint = GameObject.Find("Focal Point");
         currentEnergy = maxEnergy;
         energyBar.SetMaxEnergy(maxEnergy);
+
+        playerAnim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        float forwardInput = Input.GetAxis("Vertical");
-        float horizontalInput = Input.GetAxis("Horizontal");
-        playerRb.AddForce(focalPoint.transform.forward * forwardInput * speed);
-        playerRb.AddForce(focalPoint.transform.right * speed * horizontalInput); 
+        Movement();
 
-        
         if(Input.GetKeyDown(KeyCode.Space))
         {
             speed=1000;
@@ -37,10 +38,26 @@ public class PlayerController : MonoBehaviour
             GetTired();
         }
         Recover();
-        speed=500;
+        speed=initialSpeed;
          
     }
 
+    void Movement()
+    {
+        float forwardInput = Input.GetAxis("Vertical");
+        float horizontalInput = Input.GetAxis("Horizontal");
+        playerRb.AddForce(focalPoint.transform.forward * forwardInput * speed);
+        playerRb.AddForce(focalPoint.transform.right * speed * horizontalInput); 
+
+        //PlayerAnimation
+        if(forwardInput != 0|| horizontalInput != 0)
+        {
+            playerAnim.SetBool("Walking", true);
+        } else 
+        {
+            playerAnim.SetBool("Walking", false);
+        }
+    }
     void GetTired(){
         for(int i=0;i<maxEnergy;i++)
         {
