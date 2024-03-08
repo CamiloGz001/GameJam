985ba40c4d8a9bf828ac4a5deb9bfc24b0d9c1e3
CamiloGz001 <camilogomez1014@hotmail.com>
2024-03-07 21:09:58 -0500

    food
    food recolection

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,70 +9,77 @@ public class PlayerController : MonoBehaviour
 {
     private Rigidbody playerRb;
     public float speed;
-    public float initialSpeed =500;
+    public float initialSpeed = 10;
     private GameObject focalPoint;
-    public int maxEnergy = 100;
-    public int currentEnergy;
-    public Energy energyBar;
+    public GameObject food;
+    public bool hasFood;
     [SerializeField] private Animator playerAnim;
-    // Start is called before the first frame update
+
+    private bool isBoosting = false;
+
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
         focalPoint = GameObject.Find("Focal Point");
-        currentEnergy = maxEnergy;
-        energyBar.SetMaxEnergy(maxEnergy);
-
         playerAnim = GetComponent<Animator>();
     }
-
-    // Update is called once per frame
-    void Update()
+void Update()
     {
         Movement();
 
-        if(Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            speed=1000;
-            playerRb.AddForce(focalPoint.transform.forward * speed * Time.deltaTime);
-            GetTired();
+            isBoosting = true;
         }
-        Recover();
-        speed=initialSpeed;
-         
-    }
 
-    void Movement()
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            isBoosting = false;
+        }
+
+        if (isBoosting)
+        {
+            // Aumentar la velocidad mientras se mantiene presionada la tecla Espacio
+            speed = 15;
+        }
+        else
+        {
+            // Restaurar la velocidad inicial si la tecla Espacio no está presionada
+            speed = initialSpeed;
+        }
+    }
+void Movement()
     {
         float forwardInput = Input.GetAxis("Vertical");
         float horizontalInput = Input.GetAxis("Horizontal");
-        playerRb.AddForce(focalPoint.transform.forward * forwardInput * speed);
-        playerRb.AddForce(focalPoint.transform.right * speed * horizontalInput); 
 
-        //PlayerAnimation
-        if(forwardInput != 0|| horizontalInput != 0)
+        Vector3 forwardForce = focalPoint.transform.forward * forwardInput * speed * Time.deltaTime;
+        Vector3 horizontalForce = focalPoint.transform.right * horizontalInput * speed * Time.deltaTime;
+
+        // Aplicar fuerzas al jugador solo si hay entrada de movimiento
+        if (forwardInput != 0 || horizontalInput != 0)
         {
+            // Agregar fuerzas de movimiento
+            playerRb.AddForce(forwardForce, ForceMode.VelocityChange);
+            playerRb.AddForce(horizontalForce, ForceMode.VelocityChange);
+
+            // Rotar el jugador hacia la dirección de movimiento
+            Quaternion newRotation = Quaternion.LookRotation(forwardForce + horizontalForce);
+            playerRb.MoveRotation(newRotation);
+
+            // Animación del jugador
             playerAnim.SetBool("Walking", true);
-        } else 
+        }
+        else
         {
+            // Detener la animación si no hay movimiento
             playerAnim.SetBool("Walking", false);
         }
     }
-    void GetTired(){
-        for(int i=0;i<maxEnergy;i++)
-        {
-            currentEnergy -=i;
-            energyBar.SetEnergy(currentEnergy);
-            if(currentEnergy== 0){
-                speed = 250;
-            }
-        }
-    }
-    void Recover(){
-        for(int i=0;i<maxEnergy;i++)
-        {
-            currentEnergy +=i;
-            energyBar.SetEnergy(currentEnergy);
+private void OnTriggerEnter(Collider other) {
+        if(other.CompareTag("Food")){
+            hasFood = true;
+            food.gameObject.SetActive(true);
         }
     }
 }
